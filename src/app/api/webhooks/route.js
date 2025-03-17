c9c0162aece296580267bad6e53269b13c698ef9
mesfun <mesfun@hotmail.com>
2025-03-17 16:20:59 -0400

    update webhooks

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -2,8 +2,8 @@
 import { Webhook } from 'svix';
 
 import { headers } from 'next/headers';
-import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
-import { clerkClient } from '@clerk/nextjs/server';
+
+
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -57,50 +57,16 @@ export async function POST(req) {
   const eventType = evt?.type;
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log('Webhook body:', body);
-
-  if (eventType === 'user.created' || eventType === 'user.updated') {
-    const { id, first_name, last_name, image_url, email_addresses, username } =
-      evt?.data;
-    try {
-      const user = await createOrUpdateUser(
-        id,
-        first_name,
-        last_name,
-        image_url,
-        email_addresses,
-        username
-      );
-      if (user && eventType === 'user.created') {
-        try {
-          await clerkClient.users.updateUserMetadata(id, {
-            publicMetadata: {
-              userMongoId: user._id,
-              isAdmin: user.isAdmin,
-            },
-          });
-        } catch (error) {
-          console.log('Error updating user metadata:', error);
-        }
-      }
-    } catch (error) {
-      console.log('Error creating or updating user:', error);
-      return new Response('Error occured', {
-        status: 400,
-      });
-    }
+  
+  if (evt.type == 'user.created') {
+    console.log ('userid :', evt.data.id);
   }
 
-  if (eventType === 'user.deleted') {
-    const { id } = evt?.data;
-    try {
-      await deleteUser(id);
-    } catch (error) {
-      console.log('Error deleting user:', error);
-      return new Response('Error occured', {
-        status: 400,
-      });
-    }
-  }
+if (evt.type === 'user.updated') {
+  console.log('User updated:', evt.data.id);
+}
+
+  
 
   return new Response('', { status: 200 });
 }
